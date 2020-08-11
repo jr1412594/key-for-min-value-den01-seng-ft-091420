5e710bc532687ee0cdc8dbbbfb1d0a5654ad7a67
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-11 23:14:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-name_hash.each do |key, value|
+  hash.each do |key, value|
   if lowest_value = nil || lowest_value > value
     lowest_value = value
     lowest_key = key
